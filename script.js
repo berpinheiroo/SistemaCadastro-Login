8b336ac75c03b8ee4cf4d84372ee44ff5dc43fb4
Bernardo Pinheiro <berpinheiroo@gmail.com>
2022-10-25 22:32:24 -0300

    Criando funcao cadastro

---
+++ script.js
@@ -0,0 +1,19 @@
+var user = []
+var senha = []
+var acao = 0
+var contador = 0
+EscolhaAcao()
+
+function Cadastro(){
+ user[contador] = prompt("Qual seu nome?")
+ senha[contador] = prompt("Qual sua senha?")
+ contador++
+}
+
+function Login(user, senha){
+   
+}
+
+function EscolhaAcao(){
+   acao = prompt("O que voce quer fazer? Digite 1 para cadastro, 2 para login, 3 para excluir cadastro ou 4 para encerrar o programa")
+}
