398adae0ff2e3241249cb889be8dbfcedd91b8e5
Bernardo Pinheiro <berpinheiroo@gmail.com>
2022-10-26 21:43:51 -0300

    Adicionando a funcao de excluir o cadastro

--- script.js
+++ script.js
@@ -20,6 +20,29 @@ function Login(nome, senha) {
    }
 }
 
+var arrayUser = []
+var arraySenha = []
+var indexSup = 0
+
+function ExclusaoCadastro(nome){
+   for(var i2 = 0; i2 <= i; i2++){
+      if(nome == user[i2]){
+         user[i2] = 0
+         senhas[i2] = 0
+      }
+   }
+   for(var i3 = 0; i3 <= i2; i3++){
+      if(user[i3] != 0 && senhas[i3] != 0){
+         arrayUser[indexSup] = user[i3]
+         arraySenha[indexSup] = senhas[i3]
+         indexSup++
+      }
+   }
+   user = arrayUser
+   senhas = arraySenha
+}
+
+
 function EscolhaAcao() {
    acao = prompt("O que voce quer fazer? Digite 1 para cadastro, 2 para login, 3 para excluir cadastro ou 4 para encerrar o programa")
 }
