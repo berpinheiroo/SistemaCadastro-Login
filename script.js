bd7b5444707bcd32ec4c27c2cb91422bb05408ed
Bernardo Pinheiro <berpinheiroo@gmail.com>
2022-10-27 18:19:50 -0300

    Toques finais e fazendo o fluxo de funcoes

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 var user = []
 var senhas = []
-var acao = 0
+var opcao = 0
 var contador = 0
-EscolhaAcao()
+var continuar = "s"
+
+function EscolhaAcao() {
+   opcao = prompt("O que voce quer fazer? Digite 1 para cadastro, 2 para login, 3 para excluir cadastro ou 4 para encerrar o programa")
+}
 
 function Cadastro() {
    user[contador] = prompt("Qual seu nome?")
@@ -24,15 +28,15 @@ var arrayUser = []
 var arraySenha = []
 var indexSup = 0
 
-function ExclusaoCadastro(nome){
-   for(var i2 = 0; i2 <= i; i2++){
-      if(nome == user[i2]){
+function ExclusaoCadastro(nome) {
+   for (var i2 = 0; i2 <= contador; i2++) {
+      if (nome == user[i2]) {
          user[i2] = 0
          senhas[i2] = 0
       }
    }
-   for(var i3 = 0; i3 <= i2; i3++){
-      if(user[i3] != 0 && senhas[i3] != 0){
+   for (var i3 = 0; i3 <= contador; i3++) {
+      if (user[i3] != 0 && senhas[i3] != 0) {
          arrayUser[indexSup] = user[i3]
          arraySenha[indexSup] = senhas[i3]
          indexSup++
@@ -42,7 +46,29 @@ function ExclusaoCadastro(nome){
    senhas = arraySenha
 }
 
+while(continuar == "s"){
+   EscolhaAcao()
 
-function EscolhaAcao() {
-   acao = prompt("O que voce quer fazer? Digite 1 para cadastro, 2 para login, 3 para excluir cadastro ou 4 para encerrar o programa")
+   if (opcao == 1){
+      Cadastro()
+   } else if (opcao == 2){
+      var nome = prompt("Insira um nome para login")
+      var senha = prompt ("Insira uma senha para login")
+      var login = Login(nome, senha)
+      if(login == true){
+         console.log("Login feito com sucesso")
+      } else {
+         console.log("Nome ou senha incorreto")
+      }
+   } else if(opcao == 3){
+      var nome = prompt("Insira um nome para excluir o cadastro")
+      ExclusaoCadastro(nome)
+      continuar = "s"
+   } else if (opcao == 4){
+      continuar = "n"
+   }
+   if(continuar == "s"){
+      continuar = prompt("Deseja continuar? s ou n")
+   }
 }
+
