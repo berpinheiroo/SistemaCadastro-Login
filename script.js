e3a6a57911cebf94e4c54d00e98b3322ce7d8b97
Bernardo Pinheiro <berpinheiroo@gmail.com>
2022-10-26 21:36:07 -0300

    Adicionando a funcao login

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 var user = []
-var senha = []
+var senhas = []
 var acao = 0
 var contador = 0
 EscolhaAcao()
 
-function Cadastro(){
- user[contador] = prompt("Qual seu nome?")
- senha[contador] = prompt("Qual sua senha?")
- contador++
+function Cadastro() {
+   user[contador] = prompt("Qual seu nome?")
+   senhas[contador] = prompt("Qual sua senha?")
+   contador++
 }
 
-function EscolhaAcao(){
+function Login(nome, senha) {
+   for (var i = 0; i <= contador; i++) {
+      if (nome == user[i] && senha == senhas[i]) {
+         return true
+      } else {
+         return false
+      }
+   }
+}
+
+function EscolhaAcao() {
    acao = prompt("O que voce quer fazer? Digite 1 para cadastro, 2 para login, 3 para excluir cadastro ou 4 para encerrar o programa")
 }
